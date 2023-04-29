22b234689e588311b3e7c01eb960ac920cf35352
Pavel <pmaskarev@vk.com>
2023-04-29 10:45:21 +0300

    Modified code; fixed bugs

--- Task/Program.cs
+++ Task/Program.cs
@@ -12,10 +12,34 @@ System.Console.WriteLine("Введите количество элементов
 int len = int.Parse(Console.ReadLine());
 
 string [] Array1 = CreateArray(len);
-string [] Array2 = CreateArray(len);
+string [] Array2 = new string [Array1.Length];
 
+System.Console.WriteLine("Задан массив:");
 PrintArray(Array1);
 
+void FilterArray(string[] Array1, string[] Array2)
+{
+    int count = 0;
+    for (int i = 0; i < Array1.Length; i++)
+    {
+    if(Array1[i].Length <= 3)
+        {
+        Array2[count] = Array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+    Console.WriteLine();
+}
+FilterArray(Array1, Array2);
+System.Console.WriteLine("Изменённый массив:");
+PrintArray(Array2);
 
 
 
