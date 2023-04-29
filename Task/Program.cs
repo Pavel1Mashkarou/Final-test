36f90e9135e4bfbecbde2e94d6bb78337c0939bf
Pavel <pmaskarev@vk.com>
2023-04-29 10:38:32 +0300

    Modofied the code of the task; Added arrays from the keybord

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray(int Length)
+{
+    string [] Array = new string [Length];
+    for (int i = 0; i < Length; i++)
+    {
+        Array[i] = Console.ReadLine();
+    }
+    return Array;
+}
+
+System.Console.WriteLine("Введите количество элементов в массиве:");
+int len = int.Parse(Console.ReadLine());
+
+string [] Array1 = CreateArray(len);
+string [] Array2 = CreateArray(len);
+
+PrintArray(Array1);
+
+
+
+
+
+
+
+
+
+
